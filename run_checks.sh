be5f778d283920a2136d96f9d1fa8e36e521df30
Hugh Denman <hugh.denman@false.email.com>
2019-02-05 22:11:07 +0000

    RunChecks - run against files in the 'private' dir also.

--- run_checks.sh
+++ run_checks.sh
@@ -1,12 +1,18 @@
-for file in passing-examples/* ; do
+for file in passing-examples/*.m ; do
     echo $file
     python style_check.py $file
 done
 
 echo
 
-for file in failing-examples/* ; do
+for file in failing-examples/*.m ; do
     echo $file
     python style_check.py $file
     echo
+done
+
+
+for file in private/*.m ; do
+    echo $file
+    python style_check.py $file
 done
